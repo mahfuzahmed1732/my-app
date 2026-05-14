65773b0155be1e9e08cf8ed1c5970eb88b30a0ad
* <you@example.com>
2026-05-14 19:29:45 +0600

    fresh commit

---
+++ app/components/navbar.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { useState } from 'react';
+import Link from 'next/link';
+
+const Navbar = () => {
+  
+  return(
+      <nav className="sticky top-0 z-50 bg-purple-700 p-5 w-full flex justify-between items-center">
+        <div className="">
+          <h2 className="text-2xl font-bold text-white uppercase">My logo</h2>
+        </div>
+        <div className="flex gap-5">
+          <Link className="text-white text-md hover:text-yollow-500 transition-colors pointer" href="/">home</Link>
+          <Link className="text-white text-md hover:text-yollow-500 transition-colors pointer" href="/about">about</Link>
+          <Link className="text-white text-md hover:text-yollow-500 transition-colors pointer" href="/contact">contact</Link>
+          <Link className="text-white text-md hover:text-yollow-500 transition-colors pointer" href="/blog">blog</Link>
+        </div>
+      </nav>
+    )
+};
+
+export default Navbar;
---
+++ postcss.config.mjs
@@ -0,0 +1,8 @@
+export default config;
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
