f4dbff35d9aaa31fa764b2b5e92591099eccfdb3
yuaan123 <yuaanmahfuz123@gmail.com>
2026-05-16 14:06:49 +0600

    navbar total change

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,22 +1,35 @@
 "use client";
-import { useState } from 'react';
-import Link from 'next/link';
+
+import Link from "next/link";
 
 const Navbar = () => {
-  
-  return(
-      <nav className="sticky top-0 z-50 bg-green-700 p-5 w-full flex justify-between items-center">
-        <div className="">
-          <h2 className="text-xl md:text-2xl font-bold text-white uppercase">mahfuz.<span className="text-sm text-red-500">com</span></h2>
-        </div>
-        <div className="flex gap-5">
-          <Link className="text-white text-md hover:text-yollow-500 transition-colors pointer" href="/">home</Link>
-          <Link className="text-white text-md hover:text-yollow-500 transition-colors pointer" href="/about">about</Link>
-          <Link className="text-white text-md hover:text-yollow-500 transition-colors pointer" href="/contact">contact</Link>
-          <Link className="text-white text-md hover:text-yollow-500 transition-colors pointer" href="/blog">blog</Link>
-        </div>
-      </nav>
-    )
+  return (
+    <nav className="sticky top-0 z-50 bg-green-700 p-5 w-full flex justify-between items-center">
+      
+      <h2 className="text-xl md:text-2xl font-bold text-white uppercase">
+        mahfuz.<span className="text-sm text-red-500">com</span>
+      </h2>
+
+      <div className="flex gap-5">
+        <Link className="text-white text-md hover:text-red-500 transition-colors" href="/">
+          Home
+        </Link>
+
+        <Link className="text-white text-md hover:text-red-500 transition-colors" href="/about">
+          About
+        </Link>
+
+        <Link className="text-white text-md hover:text-red-500 transition-colors" href="/contact">
+          Contact
+        </Link>
+
+        <Link className="text-white text-md hover:text-red-500 transition-colors" href="/blog">
+          Blog
+        </Link>
+      </div>
+
+    </nav>
+  );
 };
 
 export default Navbar;
