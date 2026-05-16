57832391af9156d4f87c626c74533f4572b1ae7e
mahfuz <mahfuz11610@gmail.com>
2026-05-16 15:21:21 +0600

    my app navbar change

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,14 +4,14 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="sticky top-0 z-50 bg-green-700 p-5 w-full flex justify-between items-center">
+    <nav className="sticky top-0 z-50 bg-blue-700 p-5 w-full flex justify-between items-center">
       
       <h2 className="text-xl md:text-2xl font-bold text-white uppercase">
         mahfuz.<span className="text-sm text-red-500">com</span>
       </h2>
 
       <div className="flex gap-5">
-        <Link className="text-white text-md hover:text-red-500 transition-colors" href="/">
+        <Link className="text-white text-md hover:text-red-500 transition-colors cursor-pointer" href="/">
           Home
         </Link>
 
